1508fd1ba0213ad904309d9d6745509e6e5575ba
cyanes25 <cyanes@adacapital.com.br>
2023-11-22 11:44:40 -0300

    add fast quote

---
+++ fastQuote.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const axios = require('axios');
+const crypto = require('crypto');
+
+
+// Função para ler chave privada
+function readPrivateKey() {
+  return fs.readFileSync('private_key.pem', 'utf8');
+}
+
+// Função para ler chave pública
+function readPublicKey() {
+  return fs.readFileSync('public_key.pem', 'utf8');
+}
+
+// Função para gerar a assinatura
+function signMessage(privateKey, message) {
+  const sign = crypto.createSign('RSA-SHA256');
+  sign.update(message);
+  return sign.sign(privateKey, 'base64');
+}
+
+
+
+async function fastQuote(amount, inputCoin, outputCoin ) {
+const timestamp = Date.now().toString();
+const httpMethod='GET'
+const endpoint = `/v1/business/fast-quote?operation=swap&amount=${amount}&chain=Polygon&inputCoin=${inputCoin}&outputCoin=${outputCoin}`;
+
+const privateKey = readPrivateKey('private_key.pem');
+const message = timestamp + httpMethod + endpoint;
+const Signature = signMessage(privateKey, message);  
+
+  try {
+
+    const response = await axios.get('https://api.brla.digital:5567'+endpoint, {
+      headers: {
+        'X-API-Timestamp': timestamp,
+        'X-API-Key': '7b599d18-1794-44e4-b7f5-77a7fb9a4677',
+        'X-API-Signature': Signature,
+      },
+    });
+
+    console.log('Request successful:', response.data);
+  } catch (error) {
+    console.error('Error:', error.response.data || error.message);
+  
+   
+  }
+}
+
+fastQuote(100, 'BRLA', 'USDT');
