227828a645c98c3f90b6713613379759bf336b37
cyanes25 <cyanes@adacapital.com.br>
2025-03-06 17:49:46 -0300

    close deal

---
+++ closeOrder.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const axios = require('axios');
+
+let globalJwt;
+
+async function loginUser(email, password) {
+  const endpoint = 'https://api.brla.digital:5567/v1/business/login';
+  const body = { email, password };
+
+  try {
+    const response = await axios.post(endpoint, body);
+    globalJwt = response.data.accessToken;
+    console.log('Login successful! Token:', globalJwt);
+  } catch (error) {
+    console.error('Error during login:', error.response.data || error.message);
+  }
+}
+
+async function fastQuote(amount, inputCoin, outputCoin ) {
+  await loginUser("your_email", "your_password");
+  const endpoint = `/v1/business/fast-quote?operation=pix-to-usd&amount=${amount}&chain=Polygon&inputCoin=${inputCoin}&outputCoin=${outputCoin}`;
+
+  try {
+    const response = await axios.get('https://api.brla.digital:5567' + endpoint, {
+      headers: {
+        'Authorization': 'bearer ' + globalJwt,
+      },
+    });
+    console.log('Request successful:', response.data);
+    return response.data; // Make sure to return the data if you need it
+  } catch (error) {
+    console.error('Error:', error.response.data || error.message);
+  }
+}
+
+async function closeDeal(token) {
+  const endpoint = `/v1/business/pay-in/pix-to-usd`;
+  const body = { token };
+
+  try {
+    const response = await axios.post('https://api.brla.digital:5567' + endpoint, body, {
+      headers: {
+        'Authorization': 'bearer ' + globalJwt,
+      },
+    });
+    console.log('Request successful:', response.data);
+    return response.data; // Return the data if you need it
+  } catch (error) {
+    console.error('Error:', error.response.data || error.message);
+  }
+}
+
+// Wrap the top-level code in an async function
+(async function main() {
+  const quoteResponse = await fastQuote(100, 'BRLA', 'USDC');
+  if (quoteResponse && quoteResponse.token) {
+    const closeResponse = await closeDeal(quoteResponse.token);
+    console.log("quoteResponse: ", quoteResponse);
+    console.log("closeResponse: ", closeResponse);
+  }
+})();
